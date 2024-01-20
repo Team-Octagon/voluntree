b57cdf14656901609243bc375343747773089b72
James Ligeralde <jligeral@hawaii.edu>
2024-01-20 09:38:39 -1000

    Created VolunteerProfileInfo component

--- app/imports/ui/components/VolunteerProfileInfo.jsx
+++ app/imports/ui/components/VolunteerProfileInfo.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Col, Container, Button, Row, Image } from 'react-bootstrap';
 
 const VolunteerProfileInfo = () => (
-  <section>
-    <div>
-      <h1>Hi, my name is John</h1>
-      <p>I&apos;m a university student excited to do volunteer service!</p>
-    </div>
-  </section>
+  <Container fluid className="profile-card">
+    <Row>
+      <Col xs={12} md={{ span: 4, offset: 3 }}>
+        <Row>
+          <Col>
+            <h1>John Doe</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>I&apos;m a university looking forward to do volunteer service!</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="primary">Message</Button>
+          </Col>
+        </Row>
+      </Col>
+      <Col xs={12} md={2}>
+        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default VolunteerProfileInfo;
