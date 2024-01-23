314f01acfcf367616933e18bd24ad554fef8fa31
James Ligeralde <jligeral@hawaii.edu>
2024-01-22 16:57:11 -1000

    Create EventCollection.js

---
+++ app/imports/api/event/EventCollection.js
@@ -0,0 +1,164 @@
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import { check } from 'meteor/check';
+import { _ } from 'meteor/underscore';
+import { Roles } from 'meteor/alanning:roles';
+import BaseCollection from '../base/BaseCollection';
+import { ROLE } from '../role/Role';
+
+export const eventPublications = {
+  event: 'Event',
+  eventAdmin: 'EventAdmin',
+  eventVolunteer: 'EventVolunteer',
+};
+
+class EventCollection extends BaseCollection {
+  constructor() {
+    super('Events', new SimpleSchema({
+      name: String,
+      quantity: Number,
+      owner: String,
+    }));
+  }
+
+  /**
+   * Defines a new Event item.
+   * @param name the name of the item.
+   * @param quantity how many.
+   * @param owner the owner of the item.
+   * @return {String} the docID of the new document.
+   */
+  define({ name, quantity, owner }) {
+    const docID = this._collection.insert({
+      name,
+      quantity,
+      owner,
+    });
+    return docID;
+  }
+
+  /**
+   * Updates the given document.
+   * @param docID the id of the document to update.
+   * @param name the new name (optional).
+   * @param quantity the new quantity (optional).
+   */
+  update(docID, { name, quantity }) {
+    const updateData = {};
+    if (name) {
+      updateData.name = name;
+    }
+    // if (quantity) { NOTE: 0 is falsy so we need to check if the quantity is a number.
+    if (_.isNumber(quantity)) {
+      updateData.quantity = quantity;
+    }
+    this._collection.update(docID, { $set: updateData });
+  }
+
+  /**
+   * A stricter form of remove that throws an error if the document or docID could not be found in this collection.
+   * @param { String | Object } name A document or docID in this collection.
+   * @returns true
+   */
+  removeIt(name) {
+    const doc = this.findDoc(name);
+    check(doc, Object);
+    this._collection.remove(doc._id);
+    return true;
+  }
+
+  /**
+   * Default publication method for entities.
+   * It publishes the entire collection for admin and just the event associated to an owner.
+   */
+  publish() {
+    if (Meteor.isServer) {
+      // get the StuffCollection instance.
+      const instance = this;
+      /** This subscription publishes only the documents associated with the logged in user */
+      Meteor.publish(eventPublications.event, function publish() {
+        if (this.userId) {
+          const username = Meteor.users.findOne(this.userId).username;
+          return instance._collection.find({ owner: username });
+        }
+        return this.ready();
+      });
+
+      /** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+      Meteor.publish(eventPublications.eventAdmin, function publish() {
+        if (this.userId && Roles.userIsInRole(this.userId, ROLE.ADMIN)) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+
+      /** This subscription publishes all documents regardless of user, but only if the logged in user is a Volunteer. */
+      Meteor.publish(eventPublications.eventVolunteer, function publish() {
+        if (this.userId && Roles.userIsInRole(this.userId, ROLE.VOLUNTEER)) {
+          return instance._collection.find();
+        }
+        return this.ready();
+      });
+    }
+  }
+
+  /**
+   * Subscription method for event owned by the current user.
+   */
+  subscribeEvent() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(eventPublications.event);
+    }
+    return null;
+  }
+
+  /**
+   * Subscription method for admin users.
+   * It subscribes to the entire collection.
+   */
+  subscribeStuffAdmin() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(eventPublications.eventAdmin);
+    }
+    return null;
+  }
+
+  /**
+   * Subscription method for volunteer users.
+   * It subscribes to the entire collection.
+   */
+  subscribeEventVolunteer() {
+    if (Meteor.isClient) {
+      return Meteor.subscribe(eventPublications.eventVolunteer);
+    }
+    return null;
+  }
+
+  /**
+   * Default implementation of assertValidRoleForMethod. Asserts that userId is logged in as an Admin or User.
+   * This is used in the define, update, and removeIt Meteor methods associated with each class.
+   * @param userId The userId of the logged in user. Can be null or undefined
+   * @throws { Meteor.Error } If there is no logged in user, or the user is not an Admin or User.
+   */
+  assertValidRoleForMethod(userId) {
+    this.assertRole(userId, [ROLE.ADMIN, ROLE.USER, ROLE.VOLUNTEER]);
+  }
+
+  /**
+   * Returns an object representing the definition of docID in a format appropriate to the restoreOne or define function.
+   * @param docID
+   * @return {{owner: (*|number), quantity: *, name}}
+   */
+  dumpOne(docID) {
+    const doc = this.findDoc(docID);
+    const name = doc.name;
+    const quantity = doc.quantity;
+    const owner = doc.owner;
+    return { name, quantity, owner };
+  }
+}
+
+/**
+ * Provides the singleton instance of this class to all other entities.
+ */
+export const Events = new EventCollection();
