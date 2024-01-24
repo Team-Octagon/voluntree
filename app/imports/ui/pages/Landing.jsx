6869dad02863aee8940b8a36e8f4a4b0460c7136
Nicolas Steger <njsteger@hawaii.edu>
2024-01-23 18:45:48 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -4,48 +4,52 @@ import { Col, Container, Image, Row, Navbar, Nav, FormControl, Carousel } from '
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <Container id="landing-page" fluid className="justify-content-center d-flex">
-    <Navbar bg="white" expand="lg">
+    <Navbar bg="white" expand="sm">
       <Row className="text-center">
         <Col>
           <Container>
-            <Image src="images/Voluntree-logo.png" class="img-fluid" width="30%" />
+            <Image src="images/Voluntree-logo.png" class="img-fluid" width="20%" />
           </Container>
           <Container>
-            <Nav>
+            <Nav className="justify-content-center d-flex">
               <FormControl
                 placeholder="Search for events"
                 aria-label="Search for events"
                 aria-describedby="basic-addon2"
+                style={{ width: '40%' }}
               />
             </Nav>
           </Container>
           <Container>
             <div className="mt-4 text-center">
               <h1>Upcoming Events In Your Area</h1>
-              <p>Discover amazing content and more!</p>
+              <p>Discover volunteer events and opportunities!</p>
             </div>
-            <Image src="images/Event-list.png" />
+            <Image src="images/Event-list.png" width="80%" />
           </Container>
+          <div className="mt-4 text-center">
+            <h1>Explore Whats New</h1>
+          </div>
           <Carousel>
             <Carousel.Item>
-              <Image src="images/stock-image1.jpg" />
+              <Image src="images/stock-image1.jpg" width="60%" />
               <Carousel.Caption>
-                <h3>Gateway Cafe</h3>
-                <p>Gateway Cafe offers Breakfast and Lunch during the week.</p>
+                <h3>Volunteer</h3>
+                <p>Change the world.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src="images/stock-image2.jpg" />
+              <Image src="images/stock-image2.jpg" width="60%" />
               <Carousel.Caption>
-                <h3>Hale Aloha Café</h3>
-                <p>Hale Aloha Café serves your favorite comfort foods and cuisine influenced by global flavors. </p>
+                <h3>Volunteer</h3>
+                <p>Change the world.</p>
               </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <Image src="images/stock-image3.jpg" />
+              <Image src="images/stock-image3.jpg" width="60%" />
               <Carousel.Caption>
-                <h3>Campus Center Food Court</h3>
-                <p>The Campus Center Food Court offers a variety of food including plate lunches, bentos, grab-and-go salads and wraps, burgers, and more!</p>
+                <h3>Volunteer</h3>
+                <p>Change the world.</p>
               </Carousel.Caption>
             </Carousel.Item>
           </Carousel>
