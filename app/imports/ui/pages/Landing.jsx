6bdcd905cb7d49eb1cef1fd6c5321f93ba62e8ce
Nicolas Steger <njsteger@hawaii.edu>
2024-01-23 18:26:08 -1000

    changes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Image, Row, Navbar, Nav, FormControl } from 'react-bootstrap';
+import { Col, Container, Image, Row, Navbar, Nav, FormControl, Carousel } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
@@ -26,6 +26,29 @@ const Landing = () => (
             </div>
             <Image src="images/Event-list.png" />
           </Container>
+          <Carousel>
+            <Carousel.Item>
+              <Image src="images/stock-image1.jpg" />
+              <Carousel.Caption>
+                <h3>Gateway Cafe</h3>
+                <p>Gateway Cafe offers Breakfast and Lunch during the week.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src="images/stock-image2.jpg" />
+              <Carousel.Caption>
+                <h3>Hale Aloha Café</h3>
+                <p>Hale Aloha Café serves your favorite comfort foods and cuisine influenced by global flavors. </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image src="images/stock-image3.jpg" />
+              <Carousel.Caption>
+                <h3>Campus Center Food Court</h3>
+                <p>The Campus Center Food Court offers a variety of food including plate lunches, bentos, grab-and-go salads and wraps, burgers, and more!</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Col>
       </Row>
     </Navbar>
