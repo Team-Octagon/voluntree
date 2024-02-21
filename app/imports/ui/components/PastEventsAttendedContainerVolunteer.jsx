1f0dcf30cc0023050be13af809b7644b7d230808
micahtilton <72249867+micahtilton@users.noreply.github.com>
2024-02-20 21:19:43 -1000

    Connected events collection to the dashboard

--- app/imports/ui/components/PastEventsAttendedContainerVolunteer.jsx
+++ app/imports/ui/components/PastEventsAttendedContainerVolunteer.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import PastEventCardVolunteer from './PastEventCardVolunteer';
 import { COMPONENT_IDS } from '../utilities/ComponentIDs';
+import { Events } from "../../api/event/Events";
+import { useTracker } from 'meteor/react-meteor-data';
 
-const pastEventsData = [
-  {
-    id: 1,
-    title: 'Event 1',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    date: '2022-01-01',
-    location: 'Event Location 1',
-  },
-  {
-    id: 2,
-    title: 'Event 2',
-    description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    date: '2022-02-15',
-    location: 'Event Location 2',
-  },
-];
-const UpcomingEventsContainerVolunteer = () => (
-  <Container id={COMPONENT_IDS.DASHBOARD_PAST_EVENTS_VOLUNTEER}>
+const UpcomingEventsContainerVolunteer = () => {
+  const { pastEventsData, ready } = useTracker(() => {
+    // Get access to Event documents.
+    const subscription = Events.subscribeEventVolunteer();
+    // Determine if the subscription is ready
+    const rdy = subscription.ready();
+    // Get the Event documents
+    const items = Events.find({}).fetch();
+    return {
+      pastEventsData: items,
+      ready: rdy,
+    };
+  }, []);
+
+  return (<Container id={COMPONENT_IDS.DASHBOARD_PAST_EVENTS_VOLUNTEER}>
     {pastEventsData.map((event) => (
-      <PastEventCardVolunteer title={event.title} date={event.date} />
+        <PastEventCardVolunteer event={event}/>
     ))}
-  </Container>
-);
+  </Container>)
+}
 
 export default UpcomingEventsContainerVolunteer;
