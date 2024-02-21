960624d3d370547315246b9d33a67faab7b38fbc
micahtilton <72249867+micahtilton@users.noreply.github.com>
2024-02-20 23:15:12 -1000

    Added welcome message

--- app/imports/ui/pages/DashboardVolunteer.jsx
+++ app/imports/ui/pages/DashboardVolunteer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Meteor} from "meteor/meteor";
 import { Col, Container, Row } from 'react-bootstrap';
 import { PAGE_IDS } from '../utilities/PageIDs';
 import StatsContainerVolunteer from '../components/StatsContainerVolunteer';
@@ -6,31 +7,36 @@ import UpcomingEventsContainerVolunteer from '../components/UpcomingEventsContai
 import PastEventsAttendedContainerVolunteer from '../components/PastEventsAttendedContainerVolunteer';
 
 /* A simple static component to render some text for the landing page. */
-const DashboardVolunteer = () => (
-  <Container id={PAGE_IDS.DASHBOARD_VOLUNTEER} fluid className="container">
-    <Row>
-      <Col>
-        <h1>Welcome Back [name]!</h1>
-      </Col>
-    </Row>
-    <Row className="mt-3">
-      <Col>
-        <StatsContainerVolunteer />
-      </Col>
-    </Row>
-    <Row className="mt-4">
-      <Col>
-        <h3>Upcoming Events Scheduled</h3>
-        <UpcomingEventsContainerVolunteer />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
-        <h3>Past Events Attended</h3>
-        <PastEventsAttendedContainerVolunteer />
-      </Col>
-    </Row>
-  </Container>
-);
+
+
+const DashboardVolunteer = () => {
+    const user = Meteor.user();
+
+
+    return (<Container id={PAGE_IDS.DASHBOARD_VOLUNTEER} fluid className="container">
+        <Row>
+            <Col>
+                <h1>Welcome Back {user ? user.username : ""}!</h1>
+            </Col>
+        </Row>
+        <Row className="mt-3">
+            <Col>
+                <StatsContainerVolunteer/>
+            </Col>
+        </Row>
+        <Row className="mt-4">
+            <Col>
+                <h3>Upcoming Events Scheduled</h3>
+                <UpcomingEventsContainerVolunteer/>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <h3>Past Events Attended</h3>
+                <PastEventsAttendedContainerVolunteer/>
+            </Col>
+        </Row>
+    </Container>)
+};
 
 export default DashboardVolunteer;
