a3d940e8f1d750bd9b66ba97469b1fdf72aec084
James Ligeralde <jligeral@hawaii.edu>
2024-03-19 10:59:29 -1000

    Passed profile through all components

--- app/imports/ui/components/EditProfileCard.jsx
+++ app/imports/ui/components/EditProfileCard.jsx
@@ -1,34 +1,69 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, Container, Modal } from 'react-bootstrap';
+import { AutoForm, ErrorsField, LongTextField } from 'uniforms-bootstrap5';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import PropTypes from 'prop-types';
+import swal from 'sweetalert';
+import { updateMethod } from '../../api/base/BaseCollection.methods';
+import { VolunteerProfiles } from '../../api/user/VolunteerProfileCollection';
 
-const EditProfileCard = () => {
+const bridge = new SimpleSchema2Bridge(VolunteerProfiles._schema);
+
+const EditProfileCard = ({ profile }) => {
   const [show, setShow] = useState(false);
+  const formRef = useRef(null);
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    console.log('EditProfileCard.handleClose');
+    setShow(false);
+  };
   const handleShow = () => setShow(true);
+
+  const submit = (data) => {
+    console.log('EditProfileCard.submit', data);
+    const collectionName = VolunteerProfiles.getCollectionName();
+    const updateData = { id: profile._id, ...data };
+    console.log('EditProfileCard.submit', collectionName, updateData);
+    updateMethod.callPromise({ collectionName, updateData })
+      .then(() => {
+        swal('Success', 'Updated successfully', 'success');
+        setShow(false);
+      })
+      .catch(error => swal('Error', error.message, 'error'));
+  };
 
   return (
     <Container>
       <Button variant="primary" onClick={handleShow}>
         Edit
       </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <AutoForm ref={formRef} schema={bridge} onSubmit={submit} model={profile}>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Profile</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <LongTextField name="bio" placeholder="Bio" />
+            <ErrorsField />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button type="submit" variant="primary">
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </AutoForm>
     </Container>
   );
+};
+
+EditProfileCard.propTypes = {
+  profile: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default EditProfileCard;
--- app/imports/ui/components/VolunteerProfileCard.jsx
+++ app/imports/ui/components/VolunteerProfileCard.jsx
@@ -3,38 +3,45 @@ import PropTypes from 'prop-types';
 import { Col, Container, Row, Image } from 'react-bootstrap';
 import EditProfileCard from './EditProfileCard';
 
-const VolunteerProfileCard = ({ firstName, lastName, bio, avatar }) => (
-  <Container fluid id="profile-card" className="my-5">
-    <Row>
-      <Col xs={12} md={{ span: 4, offset: 3 }}>
-        <Row>
-          <Col>
-            <h1>{firstName} {lastName}</h1>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p>{bio}</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <EditProfileCard />
-          </Col>
-        </Row>
-      </Col>
-      <Col xs={12} md={2}>
-        <Image roundedCircle src={avatar} width="150px" />
-      </Col>
-    </Row>
-  </Container>
-);
+const VolunteerProfileCard = ({ profile }) => {
+  const { firstName, lastName, bio, avatar } = profile;
+  return (
+    <Container fluid id="profile-card" className="my-5">
+      <Row>
+        <Col xs={12} md={{ span: 4, offset: 3 }}>
+          <Row>
+            <Col>
+              <h1>{firstName} {lastName}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p>{bio}</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <EditProfileCard
+                profile={profile}
+              />
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={12} md={2}>
+          <Image roundedCircle src={avatar} width="150px" />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 VolunteerProfileCard.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  lastName: PropTypes.string.isRequired,
-  bio: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
+  profile: PropTypes.shape({
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default VolunteerProfileCard;
