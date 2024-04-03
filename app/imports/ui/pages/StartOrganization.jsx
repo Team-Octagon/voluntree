2b5cbca15d95892a89f2129cd7970fdd8de3e1b8
micahtilton <72249867+micahtilton@users.noreply.github.com>
2024-04-03 10:25:07 -1000

    Fixed ESLint errors

--- app/imports/ui/pages/StartOrganization.jsx
+++ app/imports/ui/pages/StartOrganization.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import StartOrganizationTop from '../components/StartOrganizationTop';
 import StartOrganizationBottom from '../components/StartOrganizationBottom';
-import {PAGE_IDS} from "../utilities/PageIDs";
+import { PAGE_IDS } from '../utilities/PageIDs';
 
 const StartOrganization = () => (
   <Container id={PAGE_IDS.START_ORGANIZATION} fluid className="p-0" style={{ backgroundImage: 'url("/images/landing-background.jpg")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
